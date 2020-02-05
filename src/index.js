2d9e25d034191d3c41288b8641e5ed2f0727c0f4
AjayM17 <ajaymandrawal17@gmail.com>
2020-02-06 00:44:08 +0530

    date

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import CustomCalender from './custom_calender/custom_calender'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<CustomCalender/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
